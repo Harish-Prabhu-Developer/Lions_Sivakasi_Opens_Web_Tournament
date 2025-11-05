98b3afb7c3cabaa1f052b8dcd5c775a6abcf8a00
HARISH-ANJAC <23pca121@anjaconline.org>
2025-11-05 11:18:38 +0530

    redux command  Commited

--- Admin_App/src/redux/Slices/EntriesSlice.js
+++ Admin_App/src/redux/Slices/EntriesSlice.js
@@ -1,49 +1,49 @@
-// src/redux/slices/EntriesSlice.js
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import axios from "axios";
+// // src/redux/slices/EntriesSlice.js
+// import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+// import axios from "axios";
 
 
-// ========================
-// Thunks (Async Actions)
-// ========================
-export const getHeaders = () => ({
-  headers: {
-    Authorization: `Bearer ${localStorage.getItem("token")}`,
-    "Content-Type": "application/json",
-  },
-});
+// // ========================
+// // Thunks (Async Actions)
+// // ========================
+// export const getHeaders = () => ({
+//   headers: {
+//     Authorization: `Bearer ${localStorage.getItem("token")}`,
+//     "Content-Type": "application/json",
+//   },
+// });
 
 
-export const getEventEntries = createAsyncThunk(
-  "entries/getEventEntries",
-  async ({page, limit}, { rejectWithValue }) => {
-    try {
-      const res = await axios.get(`${API_URL}/api/v1/entry/admin/entries?page=${page}&limit=${limit}`, getHeaders());
-        console.log("Response : ",res);
+// export const getEventEntries = createAsyncThunk(
+//   "entries/getEventEntries",
+//   async ({page, limit}, { rejectWithValue }) => {
+//     try {
+//       const res = await axios.get(`${API_URL}/api/v1/entry/admin/entries?page=${page}&limit=${limit}`, getHeaders());
+//         console.log("Response : ",res);
         
-      return res.data;
-    } catch (err) {
-      return rejectWithValue(
-        err.response?.data?.msg || "Failed to fetch player entries"
-      );
-    }
-  }
-);
+//       return res.data;
+//     } catch (err) {
+//       return rejectWithValue(
+//         err.response?.data?.msg || "Failed to fetch player entries"
+//       );
+//     }
+//   }
+// );
 
 
 
 
-// 🔹 Initial state
-const initialState = {
-    pagination: null,
-    loading: false,
-    error: null,
-    entries: [],
-};
+// // 🔹 Initial state
+// const initialState = {
+//     pagination: null,
+//     loading: false,
+//     error: null,
+//     entries: [],
+// };
 
 
-const EntriesSlice =createSlice({
-    name: "entries",
-    initialState,
+// const EntriesSlice =createSlice({
+//     name: "entries",
+//     initialState,
     
-});
+// });
--- Admin_App/src/redux/Store.js
+++ Admin_App/src/redux/Store.js
@@ -1,10 +1,10 @@
-import { configureStore } from '@reduxjs/toolkit'
-import EntriesSlice from './Slices/EntriesSlice';
-const Store = configureStore({
-  reducer: {
-    entries:EntriesSlice,
-  },
-});
+// import { configureStore } from '@reduxjs/toolkit'
+// import EntriesSlice from './Slices/EntriesSlice';
+// const Store = configureStore({
+//   reducer: {
+//     entries:EntriesSlice,
+//   },
+// });
 
 
-export default Store;
+// export default Store;
